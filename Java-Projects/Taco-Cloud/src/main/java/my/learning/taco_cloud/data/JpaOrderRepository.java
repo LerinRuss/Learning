ef5ca77efd7634eb37eb09c49ffdf92851cf43af
vharsekin <harvetal@gmail.com>
2022-06-07 11:35:36 +0300

    Taco Cloud from Spring in Action 5

--- Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/data/JpaOrderRepository.java
+++ Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/data/JpaOrderRepository.java
@@ -1,7 +1,16 @@
 package my.learning.taco_cloud.data;
 
+import java.util.Date;
+import java.util.List;
 import my.learning.taco_cloud.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface JpaOrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByDeliveryZip(String deliveryZip);
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date starDate, Date endDate);
+    List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+    List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<Order> readOrdersDeliveredInSeattle();
 }
