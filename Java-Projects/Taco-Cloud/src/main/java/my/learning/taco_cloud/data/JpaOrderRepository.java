6c9fcc6e5fcd5b557218109c438cd18d249dd15d
Viteker <goodkindnice@gmail.com>
2022-08-26 19:49:19 +0300

    Spring In Action 5 edition

--- Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/data/JpaOrderRepository.java
+++ Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/data/JpaOrderRepository.java
@@ -3,8 +3,9 @@ package my.learning.taco_cloud.data;
 import java.util.List;
 import my.learning.taco_cloud.Order;
 import my.learning.taco_cloud.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface JpaOrderRepository extends CrudRepository<Order, Long> {
-    List<Order> findByUserOrderByPlacedAtDesc(User user);
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/web/OrderController.java
+++ Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/web/OrderController.java
@@ -1,16 +1,16 @@
 package my.learning.taco_cloud.web;
 
-import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import my.learning.taco_cloud.Order;
 import my.learning.taco_cloud.User;
 import my.learning.taco_cloud.data.JpaOrderRepository;
-import my.learning.taco_cloud.security.JpaUserRepository;
-import org.springframework.security.core.Authentication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -18,14 +18,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 @RequiredArgsConstructor
 @Log4j2
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
     private final JpaOrderRepository orderRepository;
+    private final OrderProps orderProps;
 
     @GetMapping("/current")
     public String orderForm(Model model) {
@@ -52,8 +55,9 @@ public class OrderController {
     }
 
     @GetMapping
-    public String orderForUser(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("order", orderRepository.findByUserOrderByPlacedAtDesc(user));
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
 
         return "orderList";
     }
