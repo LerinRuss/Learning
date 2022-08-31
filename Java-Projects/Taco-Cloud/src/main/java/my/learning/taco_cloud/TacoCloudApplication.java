af868687643d5d3680e54dcc8904761b7241258f
Viteker <goodkindnice@gmail.com>
2022-08-31 14:12:03 +0300

    Spring In Action 5 edition

--- Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/TacoCloudApplication.java
+++ Java-Projects/Taco-Cloud/src/main/java/my/learning/taco_cloud/TacoCloudApplication.java
@@ -1,7 +1,13 @@
 package my.learning.taco_cloud;
 
+import my.learning.taco_cloud.data.IngredientRepository;
+import my.learning.taco_cloud.security.JpaUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +22,15 @@ public class TacoCloudApplication implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login");
+	}
+
+	@Bean
+//	@Profile("dev")
+//	@Profile({"dev", "qa"})
+	@Profile("!prod")
+	public CommandLineRunner dataLoader(IngredientRepository ingredientRepository,
+										JpaUserRepository userRepository,
+										PasswordEncoder passwordEncoder) {
+
 	}
 }
