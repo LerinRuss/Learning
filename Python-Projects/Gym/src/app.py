d337be5369cc45a7eea037d97ac03ec0603ea45a
Viteker <goodkindnice@gmail.com>
2022-09-03 23:29:44 +0300

    OpenAI Gym First Try

---
+++ Python-Projects/Gym/src/app.py
@@ -0,0 +1,16 @@
+import gym
+
+env = gym.make("LunarLander-v2")
+observation, info = env.reset(seed=42)
+
+def policy():
+    pass
+
+for _ in range(1000):
+   env.render()
+   action = policy(observation)  # User-defined policy function
+   observation, reward, done, info = env.step(action)
+
+   if done:
+      observation, info = env.reset()
+env.close()
