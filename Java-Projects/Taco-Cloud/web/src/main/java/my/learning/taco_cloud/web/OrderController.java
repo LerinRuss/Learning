c389d0ebb9689f2773334cff923ba37e9be61b6c
Viteker <goodkindnice@gmail.com>
2022-09-07 14:56:28 +0300

    Spring In Action 5 edition

---
+++ Java-Projects/Taco-Cloud/web/src/main/java/my/learning/taco_cloud/web/OrderController.java
@@ -0,0 +1,43 @@
+package my.learning.taco_cloud.web;
+
+import lombok.RequiredArgsConstructor;
+import my.learning.taco_cloud.data.OrderRepository;
+import my.learning.taco_cloud.entity.Order;
+import my.learning.taco_cloud.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderRepository orderRepository;
+    private final OrderProps props;
+
+    @GetMapping("/current")
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute Order order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
+
+        return "orderForm";
+    }
+}
