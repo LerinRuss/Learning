c2f4af980a2d88142f6ceaa2790b77b3ba708490
Viteker <goodkindnice@gmail.com>
2022-09-08 13:33:31 +0300

    Spring In Action 5 edition

--- Java-Projects/Taco-Cloud/web/src/main/java/my/learning/taco_cloud/web/DesignTacoController.java
+++ Java-Projects/Taco-Cloud/web/src/main/java/my/learning/taco_cloud/web/DesignTacoController.java
@@ -1,10 +1,16 @@
 package my.learning.taco_cloud.web;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import my.learning.taco_cloud.data.TacoRepository;
 import my.learning.taco_cloud.entity.Taco;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +30,16 @@ public class DesignTacoController {
     private final EntityLinks entityLinks;
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos() {
+    public CollectionModel<EntityModel<Taco>> recentTacos() {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        List<Taco> tacos = tacoRepository.findAll(page).getContent();
+        CollectionModel<EntityModel<Taco>> recentResources = CollectionModel.wrap(tacos);
 
-        return tacoRepository.findAll(page).getContent();
+        recentResources.add(
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(DesignTacoController.class).recentTacos())
+                        .withRel("recents"));
+
+        return recentResources;
     }
 
     @GetMapping("/{id}")
