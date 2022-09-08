e3e9727f085f6964644ecf796b9cbb6c753a27dc
Viteker <goodkindnice@gmail.com>
2022-09-08 21:34:37 +0300

    OpenAI Gym

--- Python-Projects/Gym/src/car.py
+++ Python-Projects/Gym/src/car.py
@@ -2,4 +2,18 @@ import gym
 
 env = gym.make('MountainCar-v0')
 
+num_steps = 1500
 
+obs = env.reset()
+print(f"Initial Observation {obs}")
+
+for step in range(num_steps):
+    random_action = env.action_space.sample()
+    obs, reward, done, info = env.step(random_action)
+
+    env.render()
+
+    if done:
+        env.reset()
+
+env.close()
