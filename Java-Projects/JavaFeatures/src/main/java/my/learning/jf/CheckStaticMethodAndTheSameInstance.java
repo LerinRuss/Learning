f6bf193be4f86b45894e7f25adb4e8a84e600185
Viteker <goodkindnice@gmail.com>
2022-09-12 11:18:55 +0300

    Java Features

---
+++ Java-Projects/JavaFeatures/src/main/java/my/learning/jf/CheckStaticMethodAndTheSameInstance.java
@@ -0,0 +1,12 @@
+package my.learning.jf;
+
+public class CheckStaticMethodAndTheSameInstance {
+    public static void method1(Long id, String name) {
+
+    }
+
+    // Add name to break compiler
+    public void method1(Long id) {
+
+    }
+}
